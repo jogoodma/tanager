8283255ab2872c3f7850dcce44611ee6c6c64a41
Josh Goodman <jogoodman@gmail.com>
2021-03-25 20:15:50 -0400

    Setup tailwind, build pipeline, and a simple dash app.

---
+++ app.py
@@ -0,0 +1,54 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, meta_tags=[
+    {'name': 'viewport',
+     'content': 'width=device-width, initial-scale=1.0'
+     }
+], external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.Div(children='Navbar', className='flex flex-col w-40 md:w-56 bg-indigo-200 rounded-r-3xl overflow-hidden'),
+    html.Div(children=[
+        dcc.Graph(id='graph-with-slider'),
+        dcc.Slider(
+            id='year-slider',
+            min=df['year'].min(),
+            max=df['year'].max(),
+            value=df['year'].min(),
+            marks={str(year): str(year) for year in df['year'].unique()},
+            step=None
+        )
+    ]
+        , className='p-5 w-full')
+], className='min-h-screen flex flex-row bg-gray-100')
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('year-slider', 'value'))
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name="country",
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+      './app.py',
+      './apps/**/*.py'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
